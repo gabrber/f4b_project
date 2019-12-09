42d9c3cd7ce9d9714e4d2c210588f0f328efdb36
Gabriela Berecka <gabriela.berecka@comarch.pl>
2019-12-09 10:41:29 +0100

    contact form submit

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,13 @@ const navigationItems = document.querySelector('.page-navigation__items');
 menuButton.addEventListener('click', (e) => {
     navigationItems.classList.toggle('page-navigation__items-display');
 });
+
+// form
+const contactForm = document.querySelector('.form')
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let contactFormData = new FormData(contactForm);
+    const formObj = Object.fromEntries(contactFormData);
+    console.log(formObj);
+});
